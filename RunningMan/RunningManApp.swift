b299435c2492c005119ba59762db9279071c9bfc
JamesHuang <hmjo319@qq.com>
2025-12-07 14:09:02 +0800

    chore: project initialization

---
+++ RunningMan/RunningManApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningManApp.swift
+//  RunningMan
+//
+//  Created by 黄名靖 on 2025/12/7.
+//
+
+import SwiftUI
+
+@main
+struct RunningManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
